5becb79fa9f8667ed6573ee43c9a85ab6e2c4040
Nadir Hossain <nadirhossain336.com>
2026-04-14 22:59:17 +0600

    feat: :sparkles: create dynamic ticket as pdf.

--- .history/src/App_20260414225711.jsx
+++
@@ -1,8 +0,0 @@
-// import MovieSeatPlan from "./components/MovieSeatPlan";
-
-import SeatPlan from "./components/SeatPlan";
-import Ticket from "./components/Ticket";
-
-export default function App() {
-  return <SeatPlan />;
-}
